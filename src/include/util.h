3182f89baf6e74b0408a23d1fd7dd96533ea6ea5
8dcc <8dcc.git@gmail.com>
2023-08-03 01:29:40 +0200

    Add RELATIVE2ABSOLUTE for addresses

--- src/include/util.h
+++ src/include/util.h
@@ -1,12 +1,15 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
-#define PRINT_BYTES(ptr, n)                      \
-    {                                            \
-        for (size_t i = 0; i < n; i++)           \
-            printf("%X ", *((uint8_t*)ptr + i)); \
-        putchar('\n');                           \
+#define PRINT_BYTES(ptr, n)                        \
+    {                                              \
+        for (size_t i = 0; i < n; i++)             \
+            printf("%X ", *((uint8_t*)(ptr) + i)); \
+        putchar('\n');                             \
     }
+
+/* Location of address + Size of offset + Offset */
+#define RELATIVE2ABSOLUTE(addr) (void*)((void*)addr + 4 + *(uint32_t*)addr)
 
 /*----------------------------------------------------------------------------*/
 
