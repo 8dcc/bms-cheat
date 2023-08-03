cb8be151443cdab2c4ad0c36aa8a93b8a2827478
8dcc <8dcc.git@gmail.com>
2023-08-03 14:07:39 +0200

    Make CreateInterface pointer static

--- src/util.c
+++ src/util.c
@@ -4,14 +4,26 @@
 #include "include/util.h"
 
 void* get_interface(void* handle, const char* name) {
+    typedef void* (*fn)(const char*, int*);
+    static fn CreateInterface = NULL;
+
     if (!handle) {
         fprintf(stderr, "get_interface: invalid handle for interface %s\n",
                 name);
         return NULL;
     }
 
-    typedef void* (*Func_t)(const char*, int*);
-    const Func_t CreateInterface = (Func_t)dlsym(handle, "CreateInterface");
+    /* Initialize once */
+    if (!CreateInterface) {
+        CreateInterface = (fn)dlsym(handle, "CreateInterface");
+
+        /* dlsym failed */
+        if (!CreateInterface) {
+            fprintf(stderr, "get_interface: dlsym couldn't get "
+                            "CreateInterface\n");
+            return NULL;
+        }
+    }
 
     return CreateInterface(name, NULL);
 }
